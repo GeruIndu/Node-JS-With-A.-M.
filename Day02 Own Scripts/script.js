2ab15c5d7585ab7b198cbc96626c8c92e29922e3
GeruIndu <indrajitmondal284@gmail.com>
2025-02-14 22:09:10 +0530

    npm basics

---
+++ Day02 Own Scripts/script.js
@@ -0,0 +1,4 @@
+console.log("Hey buddy!!")
+
+// In package.json file , to run xyz scripts we should run " npm run xyz " otherwise it's give error
+// But in case of start we would only run " npm start " because it's already available ..
